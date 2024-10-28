274cb2b8fb3aa1093af331bd5f0adb6cfc7c5f37
KelvinErlangga <kelvinerlanggaa@gmail.com>
2024-10-28 21:05:32 +0700

    update index.html

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,13 +22,14 @@ export default function NavBar({ onSearch }) {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Form onSubmit={handleSearchSubmit} className="flex items-center md:w-1/2 lg:w-2/3 mx-auto">
-            <FormControl type="text" placeholder="Search Movie Here" className="mr-2 ml-10 flex-1" value={searchQuery} onChange={handleSearchChange} />
-            <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
+            <FormControl type="text" placeholder="Search Movie Here" className="mr-2 ml-10 flex-1 transition-all duration-300 ease-in-out transform" value={searchQuery} onChange={handleSearchChange} />
+            <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-2 rounded">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
               </svg>
             </button>
           </Form>
+
           <Nav className="ml-auto">
             <Nav.Link href="#now_playing">Now Playing</Nav.Link>
             <Nav.Link href="#popular">Popular</Nav.Link>
